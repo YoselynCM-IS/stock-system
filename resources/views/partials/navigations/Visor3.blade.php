ea5f83534035d3bcd1ea4957c49a35027987235d
Yose30 <yos_23@live.com.mx>
2024-12-18 12:11:00 -0600

    Update Visor3.blade.php

--- resources/views/partials/navigations/Visor3.blade.php
+++ resources/views/partials/navigations/Visor3.blade.php
@@ -1,4 +1,7 @@
 <li>
-	<a class="nav-link" href="{{ route('libro.all_sistemas') }}">{{ __("Inventario") }}</a>
+	<a class="nav-link" href="{{ route('libro.all_sistemas') }}">{{ __("Inventario Gral.") }}</a>
+</li>
+<li>
+	<a class="nav-link" href="{{ route('information.libros.lista') }}">{{ __("Libros") }}</a>
 </li>
 @include('partials.navigations.logged')
