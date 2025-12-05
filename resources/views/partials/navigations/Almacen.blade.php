4c3d2cd1b635d3b131247a5234d1d6c6c1e14f6b
YoselynCM-IS <yoselynmajestice@gmail.com>
2025-12-05 10:40:35 -0600

    Titulo libros a inventario

--- resources/views/partials/navigations/Almacen.blade.php
+++ resources/views/partials/navigations/Almacen.blade.php
@@ -12,7 +12,7 @@
 </li>
 <li class="nav-item dropdown">
 	<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-		Libros <span class="caret"></span>
+		Inventario <span class="caret"></span>
 	</a>
 	<div class="dropdown-menu dropdown-menu-left" aria-labelledby="navbarDropdown">
 		<a class="dropdown-item" href="{{ route('information.libros.lista') }}">{{ __("Sistema actual") }}</a>
