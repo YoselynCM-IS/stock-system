9b1e130b2695d8ea5e92131b809b17213d11734a
YoselynCM-IS <yoselynmajestice@gmail.com>
2026-04-22 11:40:06 -0600

    Update PedidoController.php
    al preparar pedido actualizar status en representantes

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -254,6 +254,12 @@ class PedidoController extends Controller
                     });
                 }
             });
+
+            if(env('APP_NAME') == 'MAJESTIC EDUCATION'){
+                \DB::connection('representantes')->table('pedidos')
+                        ->where('numero_referencia', $pedido->numero_referencia)->update(['status' => 'EN PROCESO']);
+            }
+
             \DB::commit();
         } catch (Exception $e) {
             \DB::rollBack();
